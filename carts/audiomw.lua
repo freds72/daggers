5a1a4cbbd06ca03283c5858985f5d461b97480dd
ridgek <ridgekuhn@users.noreply.github.com>
2023-06-10 17:00:04 -0400

    ADD sfx_volume() to audiomw.lua

--- carts/audiomw.lua
+++ carts/audiomw.lua
@@ -23,12 +23,36 @@ function sfx_damp(idx, damp)
   poke(addr, new_byte)
 end
 
+---modify volume of sfx
+--
+-- @param idx {0-63} start of sfx range to remove
+-- @param v {-7-7} volume to increment/decrement by
+function sfx_volume(idx, v)
+  local sfx_addr = 0x3200 + idx * 68
+
+  --loop through all notes
+  for note_num = 0, 31 do
+    local note_addr = sfx_addr + note_num * 2
+    --get note from ram
+    local note = %note_addr
+    --get volume bits
+    local volume = (note & 0xe00) >>> 9
+    --calculate new volume
+    local newvolume = volume > 0 and mid(1, volume + v, 7) or 0
+
+    --insert new note
+    poke2(note_addr, note & 0xf1ff | (newvolume << 9))
+  end
+end
+
 --loop dampen levels
 for damp = 0, 2 do
   --loop sfx
   for i = 0, 63 do
     --set dampen level
     sfx_damp(i, damp)
+    --atennuate volume
+    sfx_volume(i, -damp)
   end
 
   --copy sfx bank
