868d5912485ef60aac6be21cc814a13f4c3ccfcc
freds <frederic.souchu@laposte.net>
2023-07-15 13:54:57 +0200

    added "take daggers" to title improved skull fountain improved text display

--- carts/main.lua
+++ carts/main.lua
@@ -382,98 +382,9 @@ function make_bullet(origin,zangle,yangle,spread)
   }
 end
 
-function mode7(p,np,light)
-  local miny,maxy,mini=32000,-32000
-  -- find extent
-  for i=1,np do
-    local y=p[i].y
-    if (y<miny) mini,miny=i,y
-    if (y>maxy) maxy=y
-  end
-
-  --data for left & right edges:
-  local lj,rj,ly,ry,lx,ldx,rx,rdx,lu,ldu,lv,ldv,ru,rdu,rv,rdv,lw,ldw,rw,rdw=mini,mini,miny-1,miny-1
-  local maxlight,pal0=light\0.066666
-  --step through scanlines.
-  if(maxy>127) maxy=127
-  if(miny<0) miny=-1
-  for y=1+miny&-1,maxy do
-    --maybe update to next vert
-    while ly<y do
-      local v0=p[lj]
-      lj+=1
-      if (lj>np) lj=1
-      local v1=p[lj]
-      -- make sure w gets enough precision
-      local y0,y1,w1=v0.y,v1.y,v1.w
-      local dy=y1-y0
-      ly=y1&-1
-      lx=v0.x
-      lw=v0.w
-      lu=v0.u*lw
-      lv=v0.v*lw
-      ldx=(v1.x-lx)/dy
-      ldu=(v1.u*w1-lu)/dy
-      ldv=(v1.v*w1-lv)/dy
-      ldw=(w1-lw)/dy
-      --sub-pixel correction
-      local dy=y-y0
-      lx+=dy*ldx
-      lu+=dy*ldu
-      lv+=dy*ldv
-      lw+=dy*ldw
-    end   
-    while ry<y do
-      local v0=p[rj]
-      rj-=1
-      if (rj<1) rj=np
-      local v1=p[rj]
-      local y0,y1,w1=v0.y,v1.y,v1.w
-      local dy=y1-y0
-      ry=y1&-1
-      rx=v0.x
-      rw=v0.w
-      ru=v0.u*rw
-      rv=v0.v*rw
-      rdx=(v1.x-rx)/dy
-      rdu=(v1.u*w1-ru)/dy
-      rdv=(v1.v*w1-rv)/dy
-      rdw=(w1-rw)/dy
-      --sub-pixel correction
-      local dy=y-y0
-      rx+=dy*rdx
-      ru+=dy*rdu
-      rv+=dy*rdv
-      rw+=dy*rdw
-    end
-    
-    -- rectfill(rx,y,lx,y,8/rw)
-    if rw>0.15 then
-      local ddx=lx-rx
-      local ddu,ddv=(lu-ru)/ddx,(lv-rv)/ddx
-      local pal1=rw>0.9375 and maxlight or (light*rw)\0.0625
-      if(pal0!=pal1) memcpy(0x5f00,0x8000|pal1<<4,16) pal0=pal1
-      local pix=1-rx&0x0.ffff
-      tline(rx,y,lx\1-1,y,(ru+pix*ddu)/rw,(rv+pix*ddv)/rw,ddu/rw,ddv/rw)
-    end
-
-    lx+=ldx
-    lu+=ldu
-    lv+=ldv
-    lw+=ldw
-    rx+=rdx
-    ru+=rdu
-    rv+=rdv
-    rw+=rdw
-  end 
-end
-
 function draw_grid(cam,light)
   light=1
   local things,m,cx,cy,cz={},cam.m,unpack(cam.origin)
-
-  -- particles texture
-  poke4(0x5f38,0x0400.0101)
 
   pal()
 
@@ -550,7 +461,6 @@ function draw_grid(cam,light)
     end
 
     -- up/down angle
-    -- todo: adjust with height*w/2 ?
     local zangles,yside=entity.zangles,0
     if zangles!=0 then
       local yangle,step=thing.yangle or 0,1/(zangles<<1)
--- carts/title.lua
+++ carts/title.lua
@@ -1,5 +1,5 @@
 -- globals
-local _entities,_sprites={},{}
+local _entities={}
 
 -- registers a new coroutine
 local _futures={}
@@ -28,8 +28,96 @@ function update_asyncs()
   end
 end
 
+function draw_things(things,cam,lightshift)
+  local lightshift=lightshift or 1
+  local m,cx,cy,cz=cam.m,unpack(cam.origin)
+  local m1,m5,m9,m2,m6,m10,m3,m7,m11=m[1],m[5],m[9],m[2],m[6],m[10],m[3],m[7],m[11]
+
+  local cache={}
+
+  local function project_array(array)
+    for i,obj in inext,array do
+      local origin=obj.origin  
+      local oy=origin[2]
+      local x,y,z=origin[1]-cx,oy-cy,origin[3]-cz
+      local ax,az=m1*x-m5*cy+m9*z,m3*x-m7*cy+m11*z
+      
+      -- draw shadows (y=0)
+      if not obj.shadeless then
+        local ay=m2*x-m6*cy+m10*z
+        if az>8 and az<128 and 0.5*ax<az and -0.5*ax<az and -0.5*ax<az and 0.5*ay<az and -0.5*ay<az then
+          -- thing offset+cam offset              
+          local w,a=64/az,atan2(x,z)
+          local a,r=atan2(x*cos(a)+z*sin(a),cy),obj.radius*w>>1
+          local x0,y0,ry=63.5+ax*w,63.5-ay*w,r*sin(a)
+          ovalfill(x0-r,y0+ry,x0+r,y0-ry)
+        end
+      end
+  
+      -- 
+      ax+=m5*oy
+      az+=m7*oy
+      local ay=m2*x+m6*y+m10*z
+      if az>8 and az<192 and 0.5*ax<az and -0.5*ax<az and 0.5*ay<az and -0.5*ay<az then
+        local w=64/az
+        cache[#cache+1]={key=w,thing=obj,x=63.5+ax*w,y=63.5-ay*w}      
+      end
+    end
+  end
+  -- 
+  -- render shadows (& collect)
+  poke(0x5f5e, 0b11111110)
+  color(1)
+  project_array(things)
+  poke(0x5f5e, 0xff)
+  
+  rsort(cache)
+
+  -- default transparency
+  palt(15,true)
+  palt(0,false)
+  -- render
+  local pal0
+  for _,item in inext,cache do        
+    local thing=item.thing
+    local pal1=min(15,(lightshift*item.key)<<4)\1
+    if(pal0!=pal1) memcpy(0x5f00,0x8000|pal1<<4,16) palt(15,true) pal0=pal1   
+    -- draw things
+    local w0,entity,origin=item.key,thing.ent,thing.origin
+    -- zangle (horizontal)
+    local dx,dz,yangles,side,xflip,yflip=cx-origin[1],cz-origin[3],entity.yangles,0
+    local zangle=atan2(dx,-dz)
+    if yangles!=0 then
+      local step=1/(yangles<<1)
+      side=((zangle-thing.zangle+0.5+step/2)&0x0.ffff)\step
+      if(side>yangles) side=yangles-(side%yangles) xflip=true
+    end
+
+    -- up/down angle
+    local zangles,yside=entity.zangles,0
+    if zangles!=0 then
+      local yangle,step=thing.yangle or 0,1/(zangles<<1)
+      yside=((atan2(dx*cos(-zangle)+dz*sin(-zangle),-cy+origin[2])-0.25+step/2+yangle)&0x0.ffff)\step
+      if(yside>zangles) yside=zangles-(yside%zangles) yflip=true
+    end
+    -- copy to spr
+    -- skip top+top rotation
+    local frame,sprites=entity.frames[(yangles+1)*yside+side+1],entity.sprites
+    local base,w,h=frame.base,frame.width,frame.height
+    for i=0,(h-1)<<6,64 do
+      poke4(i,sprites[base],sprites[base+1],sprites[base+2],sprites[base+3])
+      base+=4
+    end
+    w0*=(thing.scale or 1)
+    local sx,sy=item.x-w*w0/2,item.y-h*w0/2
+    local sw,sh=w*w0+(sx&0x0.ffff),h*w0+(sy&0x0.ffff)
+    --
+    sspr(frame.xmin,0,w,h,sx,sy,sw,sh,xflip,yflip)    
+  end
+end
+
 function menu_state(buttons,default)
-  local skulls,ent,sprites={},_entities.skull,_sprites
+  local skulls,ent={},_entities.skull
   -- leaderboard/retry
   local over_btn,clicked
   
@@ -37,13 +125,29 @@ function menu_state(buttons,default)
   clip(0,0,0,0)
   for _,btn in pairs(buttons) do
     btn.width=print(btn[1])
+    btn.x=-btn.width-2
   end
   clip()
   -- position cursor on "default"
   default=default or 1
   active_btn=buttons[default]
-  local _,x,y=unpack(active_btn)
-  local mx,my=x+active_btn.width/2,y+3
+  local _,y=unpack(active_btn)
+  local mx,my=1+active_btn.width/2,y+3
+
+  local cam=setmetatable({
+    origin=v_zero(),    
+    track=function(_ENV,_origin,_m,angles,_tilt)
+      --
+      tilt=_tilt or 0
+      m={unpack(_m)}		
+
+      -- inverse view matrix
+      m[2],m[5]= m[5], m[2]
+      m[3],m[9]= m[9], m[3]
+      m[7],m[10]=m[10],m[7]
+      
+      origin=_origin
+    end},{__index=_ENV})
 
   return
     -- update
@@ -52,7 +156,7 @@ function menu_state(buttons,default)
       -- over button?
       over_btn=-1
       for i,btn in pairs(buttons) do
-        local _,x,y=unpack(btn)          
+        local x,_,y=1,unpack(btn)          
         if mx>=x and my>=y and mx<=x+btn.width and my<=y+6 then            
           over_btn=i
           -- click?
@@ -66,14 +170,18 @@ function menu_state(buttons,default)
         end
       end
 
-      -- skull background
+      -- skull background        
+      cam:track({0,128,-64},make_m_from_euler(0,0,0)) 
+
       if #skulls<40 then
         local s=add(skulls,{
-          origin={38+rnd(48),140,0.5+rnd()/2},
-          velocity={(1-rnd(2))/12,-rnd(0.8)-0.2,0},
+          ent=_entities.skull,
+          origin={-12+rnd(24),0,-6+rnd(12)},--0.5+rnd()/2},
+          velocity={(1-rnd(2))/12,rnd(0.8)+0.2,0},
           zangle=rnd(),
           yangle=rnd(),
-          yangle_vel=rnd()/64
+          yangle_vel=rnd()/64,
+          shadeless=true
         })
         -- sort key        
         s.key=10+8*s.origin[3]
@@ -82,7 +190,7 @@ function menu_state(buttons,default)
       for i=#skulls,1,-1 do
         local s=skulls[i]
         s.origin=v_add(s.origin,s.velocity)
-        if s.origin[2]<-16 then
+        if s.origin[2]>200 then
           deli(skulls,i)          
         else
           s.yangle+=s.yangle_vel
@@ -99,28 +207,17 @@ function menu_state(buttons,default)
       fillp()
       ovalfill(r0/3,128-r0*0.95,127-r0/3,128+r0*0.95,1)
       ovalfill(r0/2,128-r0*0.75,127-r0/2,128+r0*0.75,2)
-      rsort(skulls)
-      for i=1,#skulls do
-        local s=skulls[i]
-        local yangle=(8*(s.yangle&0x0.ffff))\1
-        local yflip=false
-        if(yangle>4) yflip=true yangle=4-(yangle%4) 
-        local frame=ent.frames[5*yangle+flr(5*s.zangle)+1]
-        local mem,base=0x0,frame.base
-        for i=0,frame.height-1 do
-          poke4(mem,sprites[base],sprites[base+1],sprites[base+2],sprites[base+3])
-          mem+=64
-          base+=4
-        end
-        memcpy(0x5f00,0x8000|flr(16*s.origin[3])<<4,16) palt(15,true)
-        sspr(frame.xmin,0,frame.width,frame.height,s.origin[1]-frame.width/2,s.origin[2]-frame.height/2,frame.width,frame.height,false,yflip)
-      end
+
+      -- 
+      draw_things(skulls,cam,0.8)
+
       pal()
       
       -- draw menu & all
       for i,btn in pairs(buttons) do
-        local s,x,y=unpack(btn)        
-        arizona_print(s,x,y,i==over_btn and 1)
+        btn.x=lerp(btn.x,1,0.4)
+        local s,y=unpack(btn)        
+        arizona_print(s,btn.x,y,i==over_btn and 1)
       end
       if(active_btn.draw) active_btn:draw()
 
@@ -134,7 +231,7 @@ end
 -- main menu buttons
 local _starting
 local _main_buttons={
-  {"pLAY",1,48,cb=function()      
+  {"pLAY",48,cb=function()      
     -- avoid reentrancy
     if(_starting) return
     _starting=true
@@ -146,15 +243,15 @@ local _main_buttons={
       -- load("daggers.p8")
     end)
   end},
-  {"lEADERBOARD",1,64,
+  {"lEADERBOARD",64,
     cb=function(self) 
       leaderboard_state()
     end},
-  {"eDITOR",1,74,
+  {"eDITOR",74,
     cb=function(self) 
       load("editor.p8")
     end},
-  {"cREDITS",1,84,
+  {"cREDITS",84,
     cb=function(self) 
       credits_state()
     end}
@@ -169,7 +266,7 @@ function delayed_print(text,centered)
   end          
   return function(print)
     for i,s in ipairs(text) do
-      startx[i]=lerp(startx[i],endx[i],0.5)
+      startx[i]=lerp(startx[i],endx[i],0.4)
       print(s,startx[i],i)
     end          
   end
@@ -194,7 +291,7 @@ function leaderboard_state()
   local delay_print=delayed_print(local_scores)
 
   next_state(menu_state,{
-    {"bACK",1,111,
+    {"bACK",111,
     cb=function() 
       -- back to main menu
       next_state(menu_state, _main_buttons)
@@ -214,9 +311,9 @@ end
 
 -- credits
 function credits_state()
-  local delay_print=delayed_print({"cODE & GFX: fREDS72","mUSIC & SFX: rIDGEK", "","tHANKS TO:","sORATH","aRTYOM bRULLOV","..."},true)
+  local delay_print=delayed_print({"cODE & GFX: fREDS72","mUSIC & SFX: rIDGEK", "fONT: LITHIFY BY SOMEPEX","","tHANKS TO:","sORATH","aRTYOM bRULLOV","..."},true)
   next_state(menu_state,{
-    {"bACK",1,111,
+    {"bACK",111,
     cb=function() 
       -- back to main menu
       next_state(menu_state, _main_buttons)
@@ -254,15 +351,30 @@ function play_state()
   local angle,dangle=v_zero(),v_zero()
   local tilt=0
   local velocity=v_zero()
-  local origin=split"512,0,512"
+  local a=rnd()
+  local origin={192*cos(a),0,192*sin(a)}
   local eye_pos=v_add(origin,split"0,24,0")
+  local launching
 
   local plane={
-    {256,0,256},
-    {256,0,768},
-    {768,0,768},
-    {768,0,256},
+    {0,0,0},
+    {0,0,8},
+    {8,0,8},
+    {8,0,0},
   }
+  local function draw_radius(r,light)
+    local r2=r*r
+    memcpy(0x5f00,0x8000|(light\0.0625)*16,16)
+    for y=0,63 do
+      local yy=31.5-y
+      local d=r2-yy*yy
+      if d>=0 then
+        local x=sqrt(d)
+        sspr(32-x,32+y,2*x,1,96-x,32+y)
+      end
+    end
+  end
+
   return
     -- update
     function()
@@ -291,6 +403,17 @@ function play_state()
       local m=make_m_from_euler(unpack(angle))        
 
       cam:track(eye_pos,m,angle,tilt)
+
+      -- player close to dagger?
+      if not launching and v_len(origin,{0,0,0})<16 then
+        -- avoid reentrancy
+        launching=true
+        do_async(function()
+          -- todo: fade to red? black?
+          wait_async(10)
+          load("daggers.p8")
+        end)
+      end
     end,
     -- draw
     function()
@@ -298,8 +421,9 @@ function play_state()
       local m,cx,cy,cz=cam.m,unpack(cam.origin)
       local m1,m5,m9,m2,m6,m10,m3,m7,m11=m[1],m[5],m[9],m[2],m[6],m[10],m[3],m[7],m[11]
       local verts,outcode,nearclip={},0xffff,0  
+      local r0=-4*16
       for i,v0 in inext,plane do
-        local code,x,y,z=2,v0[1]-cx,v0[2]-cy,v0[3]-cz
+        local code,x,y,z=2,r0+16*v0[1]-cx,v0[2]-cy,r0+16*v0[3]-cz
         local ax,ay,az=m1*x+m5*y+m9*z,m2*x+m6*y+m10*z,m3*x+m7*y+m11*z
         if(az>8) code=0
         if(az>384) code|=1
@@ -341,10 +465,30 @@ function play_state()
         end
 
         -- texture
-        poke4(0x5f38,0x3c00.0404)   
-        tline(17)   
+        poke4(0x5f38,0x3800.0808)   
+
+        -- light effect
+        poke(0x5F55,0x00)
+        local r=abs(cos(time()/8))
+        draw_radius(32-r*r,0.5)
+        r+=0.2
+        draw_radius(32-r*r,0.7)
+        r+=0.6
+        draw_radius(32-r*r,0.99)
+        poke(0x5F55,0x60) 
+
         mode7(verts,#verts,1)        
       end
+
+      draw_things({
+        {
+          ent=_entities.dagger,
+          origin={0,24,0},
+          yangle=0,
+          zangle=0,
+          radius=12
+        }
+      },cam)
 
       -- tilt!
       -- screen = gfx
@@ -364,6 +508,14 @@ function play_state()
       memset(0x6000,0,512)
       memset(0x7e00,0,512)
 
+      --[[
+      local s="HUM...cURSED?"
+      print(s,64-print(s,0,130)/2,2,6)
+      
+      local s="cOMING SOON ON PICO8..."
+      print(s,64-print(s,0,130)/2,122,8)
+      ]]
+
       pal({128, 130, 133, 5, 134, 6, 7, 136, 8, 138, 139, 3, 131, 1, 135, 0},1)
     end
 end
@@ -371,6 +523,7 @@ end
 -- entry points
 function _init()
   -- custom font
+  -- source: https://somepx.itch.io/humble-fonts-tiny-ii
   ?"\^@56000800⁴⁸⁶\0\0¹\0\0\0\0\0\0\0\0\0\0\0 \0²\0\0\0\0¹■■■■\0\0\0▮¹■■▒■ ■■■」!■\0\0\0▮■■▮\0■!■■■■!■\0\0²\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0⁷⁷⁷⁷⁷\0\0\0\0⁷⁷⁷\0\0\0\0\0⁷⁵⁷\0\0\0\0\0⁵²⁵\0\0\0\0\0⁵\0⁵\0\0\0\0\0⁵⁵⁵\0\0\0\0⁴⁶⁷⁶⁴\0\0\0¹³⁷³¹\0\0\0⁷¹¹¹\0\0\0\0\0⁴⁴⁴⁷\0\0\0⁵⁷²⁷²\0\0\0\0\0²\0\0\0\0\0\0\0\0¹²\0\0\0\0\0\0³³\0\0\0⁵⁵\0\0\0\0\0\0²⁵²\0\0\0\0\0\0\0\0\0\0\0\0\0²²²²\0²\0\0\n⁵\0\0\0\0\0\0\n゜\n゜⁸\0\0\0⁷³⁶⁷²\0\0\0⁵⁴²¹⁵\0\0\0\0⁴²◀\t◀\0\0²¹\0\0\0\0\0\0²¹¹¹¹²\0\0²⁴⁴⁴⁴²\0\0⁵²⁷²⁵\0\0\0\0²⁷²\0\0\0\0\0\0\0²¹\0\0\0\0\0⁷\0\0\0\0\0\0\0\0\0²\0\0\0⁴²²²¹\0\0\0⁶\t\rᵇ⁶\0\0\0²³²²⁷\0\0\0⁷ᶜ⁶¹ᶠ\0\0\0⁷ᶜ⁶⁸ᶠ\0\0\0⁵⁵ᶠ⁴⁴\0\0\0ᶠ¹⁶ᶜ⁷\0\0\0⁴²⁷\t⁶\0\0\0ᶠ⁸⁴²²\0\0\0⁶\t⁶\t⁶\0\0\0⁶\tᵉ⁴²\0\0\0\0²\0²\0\0\0\0\0²\0²¹\0\0\0⁴²¹²⁴\0\0\0\0⁷\0⁷\0\0\0\0¹²⁴²¹\0\0\0²⁵⁴²\0²\0\0²⁵⁵¹⁶\0\0\0\0⁶⁸ᵇ⁶\0\0\0¹⁵\t\t⁶\0\0\0\0⁶¹¹⁶\0\0\0⁸\n\t\t⁶\0\0\0\0ᵉ\t⁵ᵉ\0\0\0ᶜ²ᵉ³²¹\0\0\0ᵉ\t\r\n⁴\0\0¹⁵ᵇ\t\t⁴\0\0²\0³²²⁷\0\0\0ᶜ⁸⁸\t⁶\0\0¹\t⁵ᵇ\t⁴\0\0¹¹¹¹⁶\0\0\0\0\n▶‖‖\0\0\0\0⁶\t\t\t\0\0\0\0⁶\t\t⁶\0\0\0\0⁶\t\t⁵¹\0\0\0⁶\t\t\n⁸\0\0\0\rᵇ¹¹\0\0\0\0ᵉ³⁸ᶠ\0\0\0\0²ᵉ³²ᶜ\0\0\0\t\t\t⁶\0\0\0\0\t\t⁵³\0\0\0\0‖‖‖ᵇ\0\0\0\0\t⁶⁴\t\0\0\0\0\t\tᵇ⁴³\0\0\0⁷⁴²⁷\0\0\0³¹¹¹¹³\0\0¹¹³²²\0\0\0⁶⁴⁴⁴⁴⁶\0\0²⁵\0\0\0\0\0\0\0\0\0\0⁷\0\0\0²⁴\0\0\0\0\0\0⁶\tᵇ\r\t\t\0\0⁶\t⁵ᵇ\t⁷\0\0⁶\t¹¹\t⁶\0\0³⁵\t\t\t⁷\0\0⁶¹⁵³\t⁶\0\0⁶¹⁵³¹¹\0\0⁶¹¹\r\t⁶\0\0⁵⁵⁵⁷⁵⁵\0\0⁷²²²²⁷\0\0ᵉ⁸⁸⁸\t⁶\0\0\t\t⁵ᵇ\t\t\0\0²¹¹¹\t⁷\0\0\n▶‖‖‖‖\0\0\nᵇ\r\t\t\t\0\0⁶\t\t\t\t⁶\0\0⁶\t\t\r¹¹\0\0⁶\t\t\t\r\n\0\0⁶\t\t⁵ᵇ\t\0\0ᵉ³⁶⁸⁸⁷\0\0ᶜ³²²²²\0\0\t\t\t\t\t⁶\0\0\t\t\t\t⁵³\0\0‖‖‖‖▶\r\0\0\t\t\t⁶\t\t\0\0\t\t\tᵇ⁴³\0\0⁷⁴²¹¹⁷\0\0⁶²³²⁶\0\0\0²²²²²\0\0\0³²⁶²³\0\0\0\0²‖ᶜ\0\0\0\0\0²⁵²\0\0\0\0○○○○○\0\0\0U*U*U\0\0\0、>*⁘、\0\0\0>ccw>\0\0\0■D■D■\0\0\0⁴<、゛▮\0\0\0⁸*>、、⁸\0\0006>>、⁸\0\0\0、\"*\"、\0\0\0、、>、⁘\0\0\0、>○*:\0\0\0>gcg>\0\0\0○]○A○\0\0\0008⁸⁸ᵉᵉ\0\0\0>ckc>\0\0\0⁸、>、⁸\0\0\0\0\0U\0\0\0\0\0>scs>\0\0\0⁸、○>\"\0\0\0「$JZ$「\0\0>wcc>\0\0\0\0⁵R \0\0\0\0\0■*D\0\0\0\0>kwk>\0\0\0○\0○\0○\0\0\0UUUUU\0\0\0ᵉ⁴゛-&\0\0\0■!!%²\0\0\0ᶜ゛  、\0\0\0⁸゛⁸$¥\0\0\0N⁴>E&\0\0\0\"_□□\n\0\0\0゛⁸<■⁶\0\0\0▮ᶜ²ᶜ▮\0\0\0\"z\"\"□\0\0\0゛ \0²<\0\0\0⁸<▮²ᶜ\0\0\0²²²\"、\0\0\0⁸>⁸ᶜ⁸\0\0\0□?□²、\0\0\0<▮~⁴8\0\0\0²⁷2²2\0\0\0ᶠ²ᵉ▮、\0\0\0>@@ 「\0\0\0>▮⁸⁸▮\0\0\0⁸8⁴²<\0\0\0002⁷□x「\0\0\0zB²\nr\0\0\0\t>Kmf\0\0\0¥'\"s2\0\0\0<JIIF\0\0\0□:□:¥\0\0\0#b\"\"、\0\0\0ᶜ\0⁸*M\0\0\0\0ᶜ□!@\0\0\0}y■=]\0\0\0><⁸゛.\0\0\0⁶$~&▮\0\0\0$N⁴F<\0\0\0\n<ZF0\0\0\0゛⁴゛D8\0\0\0⁘>$⁸⁸\0\0\0:VR0⁸\0\0\0⁴、⁴゛⁶\0\0\0⁸²> 、\0\0\0\"\"& 「\0\0\0>「$r0\0\0\0⁴6,&d\0\0\0>「$B0\0\0\0¥'\"#□\0\0\0ᵉd、(x\0\0\0⁴²⁶+」\0\0\0\0\0ᵉ▮⁸\0\0\0\0\n゜□⁴\0\0\0\0⁴ᶠ‖\r\0\0\0\0⁴ᶜ⁶ᵉ\0\0\0> ⁘⁴²\0\0\0000⁸ᵉ⁸⁸\0\0\0⁸>\" 「\0\0\0>⁸⁸⁸>\0\0\0▮~「⁘□\0\0\0⁴>$\"2\0\0\0⁸>⁸>⁸\0\0\0<$\"▮⁸\0\0\0⁴|□▮⁸\0\0\0>   >\0\0\0$~$ ▮\0\0\0⁶ &▮ᶜ\0\0\0> ▮「&\0\0\0⁴>$⁴8\0\0\0\"$ ▮ᶜ\0\0\0>\"-0ᶜ\0\0\0、⁸>⁸⁴\0\0\0** ▮ᶜ\0\0\0、\0>⁸⁴\0\0\0⁴⁴、$⁴\0\0\0⁸>⁸⁸⁴\0\0\0\0、\0\0>\0\0\0> (▮,\0\0\0⁸>0^⁸\0\0\0   ▮ᵉ\0\0\0▮$$DB\0\0\0²゛²²、\0\0\0>  ▮ᶜ\0\0\0ᶜ□!@\0\0\0\0⁸>⁸**\0\0\0> ⁘⁸▮\0\0\0<\0>\0゛\0\0\0⁸⁴$B~\0\0\0@(▮h⁶\0\0\0゛⁴゛⁴<\0\0\0⁴>$⁴⁴\0\0\0、▮▮▮>\0\0\0゛▮゛▮゛\0\0\0>\0> 「\0\0\0$$$ ▮\0\0\0⁘⁘⁘T2\0\0\0²²\"□ᵉ\0\0\0>\"\"\">\0\0\0>\" ▮ᶜ\0\0\0> < 「\0\0\0⁶  ▮ᵉ\0\0\0\0‖▮⁸⁶\0\0\0\0⁴゛⁘⁴\0\0\0\0\0ᶜ⁸゛\0\0\0\0、「▮、\0\0\0⁸⁴c▮⁸\0\0\0⁸▮c⁴⁸\0\0\0"
   -- enable custom font
   -- enable tile 0 + extended memory
@@ -403,12 +556,24 @@ cartdata;freds72_daggers]],exec)
   --
   -- load background assets
   decompress("pic",0,0,function()
-      -- drop array size
-      mpeek2()
-      local id,angles=mpeek(),mpeek()
-      _entities.skull={        
-        frames=unpack_frames(_sprites)
+    local names={
+      [1]="skull",
+      [7]="dagger"
+    }
+    -- drop array size
+    for i=1,mpeek2() do
+      local id,sprites,angles=mpeek(),{},mpeek()    
+      local ent={  
+        sprites=sprites,   
+        yangles=angles&0xf,
+        zangles=angles\16,        
+        frames=unpack_frames(sprites)
       }
+      local name=names[id]
+      if name then
+        _entities[name]=ent
+      end
+    end
   end)
   reload()
 
