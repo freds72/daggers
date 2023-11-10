e2cef1e96acc9910d3bbbee177aacc39a9bc0562
ridgek <ridgekuhn@users.noreply.github.com>
2023-11-09 21:22:14 -0500

    UPDATE - Reorganize sfx in sfx.p8

--- carts/main.lua
+++ carts/main.lua
@@ -79,7 +79,7 @@ function levelup_async(t)
   for j=0.125,t<<2,0.125 do
     _ramp_pal=0x8280+((-127*sin(j/(t<<3)))&15)*16
     _slow_mo+=1    
-    if(j==t<<1) sfx"43"
+    if(j==t<<1) sfx"56"
     yield()
   end
 
@@ -213,13 +213,13 @@ function make_player(_origin,_a)
       -- double-click detector
       dblclick_ttl=max(dblclick_ttl-1)
       if not btn(@0xc415) then
-        sfx(48, -2)
+        sfx(60, -2)
         if not fire_released then
           if dblclick_ttl>0  then
             -- double click timer still active?
             -- shotgun (repulsive!)
             fire_ttl,fire,dblclick_ttl,attract_power=0,2,0,-1
-            sfx(29+_piercing, stat"57" and -2 or flr(rnd"4"))
+            sfx(61+_piercing, stat"57" and -2 or flr(rnd"4"))
           elseif fire_frames<4 then
             -- candidate for double click?
             dblclick_ttl=8
@@ -233,7 +233,7 @@ function make_player(_origin,_a)
         fire_frames+=1
         -- regular fire      
         if dblclick_ttl==0 and fire_ttl<=0 then
-          sfx(48, stat"57" and -2)
+          sfx(60, stat"57" and -2)
           fire_ttl,fire=_fire_ttl,1
         end
         -- 
@@ -615,7 +615,7 @@ function make_skull(_ENV,_origin)
   grid_register(_ENV)
   
   --play spawn sfx
-  sfx(spawnsfx or 40)
+  sfx(spawnsfx or 29)
 
   return _ENV
 end
@@ -633,7 +633,7 @@ function make_spider()
         end
         -- unregister
         dead,_spiders[_ENV]=true
-        sfx"52"
+        sfx"35"
       end
     end,
     update=function(_ENV)
@@ -703,12 +703,12 @@ function make_squid(type)
             if jewel and _dist==1 then
               -- feedback
               make_particle(_lgib_template,pos,{u,-3*bullet.velocity[2],v})
-              sfx"56"
+              sfx"59"
               if register_hit(_ENV) then
                 make_jewel(origin,{u,3,v},16)
                 -- change appearance + avoid reentrancy (jewel can't be null!!)
                 ent,jewel=_entities.squid2,false
-                if(type==1) _dead=1 music"39"
+                if(type==1) _dead=1 music"28"
                 -- "downgrade" squid!!
                 type-=1
               end
@@ -769,7 +769,7 @@ function make_worm(type)
   head=make_skull(inherit({
     die=function(_ENV)
       sfx"-1"
-      music"54"
+      music"37"
       -- clean segment
       do_async(function()
         while #segments>0 do
@@ -842,7 +842,7 @@ function make_worm(type)
     post_think=function(_ENV)
       local curr_y,dirt=origin[2]
       if sgn(curr_y)!=sgn(prev_y) then
-        sfx"59"
+        sfx"39"
         make_dirt(_ENV)
         dirt=true
       end
@@ -942,7 +942,7 @@ function make_egg(_origin,_velocity)
             init=function()
               -- kill egg
               local _ENV=egg
-              sfx"51"
+              sfx"34"
               dead=true
               for i=1,2+rnd"2" do
                 local a=rnd()
@@ -1096,7 +1096,7 @@ stat;0]]
     -- init
     function()
       exec[[sfx;-1
-music;32
+music;44
 set;_hw_pal;0x8000]]
       -- must be done *outside* async update loop!!!
       _futures,_total_things,_total_time,_time_inc={},0,0,0x0.0001
@@ -1110,7 +1110,7 @@ set;_hw_pal;0x8000]]
         end
         exec[[wait_async;90
 set;dead;1;_plyr
-next_state;gameover_state;lIBERATED;256;0.01;61]]
+next_state;gameover_state;lIBERATED;256;0.01;41]]
       end)
 
     -- progression
@@ -1199,7 +1199,7 @@ function gameover_state(obituary,height,height_attract,music_id)
   -- time format v2
   exec[[sfx;-1
 dset;0;2]]
-  music(music_id or 36)
+  music(music_id or 48)
 
   -- number of scores
   dset(1,#_local_scores)
@@ -1513,7 +1513,7 @@ tline;17]]
         if die then
           die(_ENV)
         else
-          sfx(death_sfx or 52)
+          sfx(death_sfx or 35)
         end
         -- drop jewel?
         if jewel then
@@ -1632,11 +1632,11 @@ _worm_seg_giga1;;_worm_seg_tail1
 _worm_seg_normal2;;_worm_seg_tail2
 _worm_seg_mega2;;_worm_seg_tail2
 _worm_seg_giga2;;_worm_seg_tail2
-_worm_head_normal;hit_ttl,0,wobble0,9,wobble1,12,seed0,5,seed1,6,ent,worm0,s_radius,12,radius,16,hp,50,chatter,20,spawnsfx,42,obituary,sLICED,ground_limit,-64,cost,10,gibs,0.5,templates,0|0|0|0|0|0|0|0|0|0|1|2;_skull_template
-_worm_head_mega;hit_ttl,0,wobble0,8,wobble1,11,seed0,3,seed1,4.5,ent,worm0,s_radius,14,radius,20,scale,1.2,hp,200,chatter,20,spawnsfx,42,obituary,mINCED,ground_limit,-64,cost,15,gibs,0.7,templates,0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|1|2;_skull_template
-_worm_head_giga;hit_ttl,0,wobble0,7,wobble1,10,seed0,2,seed1,3.5,ent,worm0,s_radius,16,radius,22,scale,1.5,hp,400,chatter,20,spawnsfx,42,obituary,gUTTED,ground_limit,-64,cost,20,gibs,1,templates,0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|1|2;_skull_template
+_worm_head_normal;hit_ttl,0,wobble0,9,wobble1,12,seed0,5,seed1,6,ent,worm0,s_radius,12,radius,16,hp,50,chatter,20,spawnsfx,31,obituary,sLICED,ground_limit,-64,cost,10,gibs,0.5,templates,0|0|0|0|0|0|0|0|0|0|1|2;_skull_template
+_worm_head_mega;hit_ttl,0,wobble0,8,wobble1,11,seed0,3,seed1,4.5,ent,worm0,s_radius,14,radius,20,scale,1.2,hp,200,chatter,20,spawnsfx,31,obituary,mINCED,ground_limit,-64,cost,15,gibs,0.7,templates,0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|1|2;_skull_template
+_worm_head_giga;hit_ttl,0,wobble0,7,wobble1,10,seed0,2,seed1,3.5,ent,worm0,s_radius,16,radius,22,scale,1.5,hp,400,chatter,20,spawnsfx,31,obituary,gUTTED,ground_limit,-64,cost,20,gibs,1,templates,0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|1|2;_skull_template
 _jewel_template;reg,1,ent,jewel,s_radius,8,radius,12,zangle,0,ttl,300,@apply,nop
-_spiderling_template;ent,spiderling0,radius,8,hp,2,on_ground,1,death_sfx,53,chatter,16,obituary,wEBBED,apply_filter,on_ground,@lgib,_goo_template,ground_limit,2;_skull_template
+_spiderling_template;ent,spiderling0,radius,8,hp,2,on_ground,1,death_sfx,36,chatter,16,obituary,wEBBED,apply_filter,on_ground,@lgib,_goo_template,ground_limit,2;_skull_template
 _squid_core;no_render,1,s_radius,18,radius,24,origin,v_zero,on_ground,1,is_squid_core,1,min_velocity,0.2,chatter,8,@hit,nop,cost,5,obituary,nAILED,gibs,0.8,apply_filter,is_squid_core;_skull_template
 _squid_hood;bright,0,ent,squid2,radius,12,origin,v_zero,zangle,0,@apply,nop,obituary,nAILED,shadeless,1,o_offset,18,y_offset,24,r_offset,8
 _squid_jewel;bright,0,hit_ttl,0,reg,1,jewel,1,hp,7,ent,squid1,radius,8,origin,v_zero,zangle,0,@apply,nop,obituary,nAILED,shadeless,1,o_offset,18,y_offset,24,r_offset,8
@@ -1644,8 +1644,8 @@ _squid_tentacle;bright,0,ent,tentacle0,origin,v_zero,zangle,0,is_tentacle,1,shad
 _skull_base_template;;_skull_template
 _skull1_template;chatter,12,ent,skull,radius,8,hp,2,obituary,bUMPED,target_yangle,0.1;_skull_base_template
 _skull2_template;chatter,12,ent,reaper,radius,10,hp,4,seed0,5.5,seed1,6,jewel,1,obituary,iMPALED,min_velocity,3.5,gibs,0.2;_skull_base_template
-_spider_template;bright,0,ent,spider1,radius,24,shadeless,1,hp,12,spawnsfx,41,chatter,24,zangle,0,yangle,0,scale,1.5,@apply,nop;_skull_base_template
-_mine_template;ent,mine,radius,12,hp,200,spawnsfx,49,death_sfx,53,obituary,pOISONED,@apply,nop,@lgib,_goo_template,gibs,0,ground_limit,12;_skull_template]],
+_spider_template;bright,0,ent,spider1,radius,24,shadeless,1,hp,12,spawnsfx,30,chatter,24,zangle,0,yangle,0,scale,1.5,@apply,nop;_skull_base_template
+_mine_template;ent,mine,radius,12,hp,200,spawnsfx,32,death_sfx,53,obituary,pOISONED,@apply,nop,@lgib,_goo_template,gibs,0,ground_limit,12;_skull_template]],
   function(name,template,parent)
     _ENV[name]=inherit(with_properties(template),_ENV[parent])
   end)
@@ -1834,7 +1834,7 @@ poke;0x5f5e;0b11110110]]
         end
       end
 
-      if(ambient) sfx"28"
+      if(ambient) sfx"51"
     end
 ::end_noise::   
 
